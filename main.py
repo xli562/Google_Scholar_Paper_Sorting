1643e88eb8c8f9854c78e920fc973121aa1cef43
Xiaoyang Li <xl562@cam.ac.uk>
2024-02-19 19:26:49 +0000

    added gitignore

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def is_constantinides_among_first_three(authors_div):
     authors_text = authors_div.text
     authors_list = authors_text.split(", ")
     # Check if 'GA Constantinides' is among the first three authors
-    return any('GA Constantinides' in author for author in authors_list[:3])
+    return any('GA Constantinides' in author for author in authors_list[:4])
 
 # Prepare a list to hold the filtered data
 filtered_data = []
@@ -26,9 +26,9 @@ filtered_data = []
 for row in rows:
     # Check if the publication year is 2010 or later
     year_span = row.find('span', class_='gsc_a_h gsc_a_hc gs_ibl')
-    if year_span and int(year_span.text if year_span.text != '' else 0) >= 2010:
+    if year_span and int(year_span.text if year_span.text != '' else 0) >= 2018:
         title_a = row.find('a', class_='gsc_a_at')
-        if title_a and 'architecture' in title_a.text:
+        if title_a and '' in title_a.text:
             authors_div = row.find('div', class_='gs_gray')
             if is_constantinides_among_first_three(authors_div):
                 # Extract the desired information
@@ -36,7 +36,7 @@ for row in rows:
                 authors = authors_div.text if authors_div else "No authors"
                 year = year_span.text
                 citations_a = row.find('a', class_='gsc_a_ac gs_ibl')
-                citations = int(citations_a.text) if citations_a else 0
+                citations = int(citations_a.text) if citations_a.text != '' else 0
                 # Add the extracted information to the list
                 filtered_data.append([title, authors, year, citations])
 
@@ -44,4 +44,4 @@ for row in rows:
 df = pd.DataFrame(filtered_data, columns=['Title', 'Authors', 'Year', 'Citations'])
 
 # Write the DataFrame to an Excel file
-df.to_excel('filtered_articles_arch.xlsx', index=False, engine='openpyxl')
+df.to_excel('filtered_articles_newer.xlsx', index=False, engine='openpyxl')
